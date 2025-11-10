e35db18f8e8956f169ff16a634915572e28cd7b1
nicole-andrews <nicolemariaandrews@gmail.com>
2025-11-10 17:21:15 -0600

    java spring boot base added

---
+++ pantry/src/test/java/com/digitalpantry/pantry-rand/PantryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalpantry.pantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
